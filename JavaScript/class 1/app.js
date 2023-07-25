7ccdde2ad3594ea0650dba50e6006405a46bc287
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-07-25 20:40:59 +0500

    js class 1

---
+++ JavaScript/class 1/app.js
@@ -0,0 +1,76 @@
+// alert('hello world')
+// alert('hello world 2')
+
+// document.write("<h1>Makki <br> Raza</h1>")
+
+// alert(hello)
+
+// var firstName = "Mohammad Saad "
+// var lastName = "Ali"
+// alert(firstName+" "+lastName) 
+
+// var name = "a"
+// var name2 = "b"
+// var name3 = "c"
+// var name4 = "d"
+
+// alert(name1+name2)
+// document.write(name3+name4)
+
+
+
+
+
+// Datatypes
+
+// string collection of alphabets
+// integer ==> Number
+
+
+// var _num1 = "9";
+// var num2 = "4"
+
+// document.write(num1 - num2)
+var num1 = "5"
+var num2 = 10
+// var add = num1 + num2
+// var sub = num1 - num2
+// var div = num1 / num2
+// var mul = num1 * num2
+
+// document.write(add,"<br>",sub,"<br>",div,"<br>",mul,"<br>")
+
+
+var a = +prompt('KUCH BHI')
+var a = prompt('KUCH BHI')
+document.write(typeof(a))
+
+
+
+
+
+
+
+calculator
+
+prompt()
+add 
+sub 
+mul
+div 
+
+print in <table></table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
