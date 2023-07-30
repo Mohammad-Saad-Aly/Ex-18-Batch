5dafaef98c8e30aa83b0f73856b0208e3625586f
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-07-30 19:09:57 +0500

    js class 3

---
+++ JavaScript/class 3/app.js
@@ -0,0 +1,164 @@
+// &&  and operator  
+// ||  or operator
+
+// var num1 = 5
+// var num2 = "5"
+
+// if(num1 == 5 || num2 === 5){
+//     document.write('okay')
+// }
+// else{
+//     document.write('not okay')
+// }
+
+
+// false && true
+
+// 7  > 10 && 8 > 3
+
+
+
+// document.write(6 > 6 && 9 > 9)
+
+// var weight = 300
+// var time = 4
+
+
+
+// var num = 5
+// var num2 = 10
+
+// if(num == 6){
+//     if(num2 == 10){
+//         document.write('okay')
+//     }
+// }
+
+
+
+// var num = 5
+// var num2 = 10
+// var nu3 = 14
+// var nu4 = 13
+// var num5 = 16
+// var num7 = 19
+
+// document.write(num)
+
+
+// 5 variable 
+// 1 variable jo k array hoga [5 varibale ismain pass honge]
+
+// arry ko print kardeinge
+
+
+
+
+// var a = prompt('Enter value')
+// var b = prompt('Enter value')
+// var c = prompt('Enter value')
+// var d = prompt('Enter value')
+
+// var arr = [a,b,c,d]
+
+// document.write(arr)
+
+
+// var abc;
+// abc = 'Saad'
+
+document.write(abc)
+
+
+
+// var arr = []
+// arr.push('jalal')
+
+
+
+
+// var arr = ['Saad', 24, 'CSC-20S-088', 'SMIU','ranchorline']
+
+
+// arr.splice(2,1)
+// document.write(arr)
+// var arr2 = arr.slice(2,4)
+
+// document.write(arr2)
+
+// arr.splice(2, 2, 'jalal')
+
+
+// document.write(arr)
+// arr.unshift('Hassaan')
+
+// arr.shift()
+
+
+
+// arr.push('Mujtaba','Amaan')
+
+// arr.pop()
+// arr.pop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
