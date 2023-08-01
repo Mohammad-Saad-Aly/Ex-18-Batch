88abe1737a68369173fba6c1c144f29420e482b5
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-08-01 21:01:25 +0500

    js class 4

---
+++ JavaScript/tasks/app.js
@@ -0,0 +1,187 @@
+// var userInput = +prompt('Enter value');
+
+// if(userInput % 2 === 0){
+//     alert('Even Number')
+// }
+// else{
+//     alert('Odd Number')
+// }
+
+
+// user ka naam 
+// english 
+// isl 
+// bio
+// chem
+// pst
+// total === uske baad uski percentage nikle gi
+// or jab perc nikal jaegi to banega grade 
+// 80 se upar ya brabar A+
+// 70 se upar A
+// 60 se upar B
+
+// table ki form main 
+
+// num1 = prompt 
+// num2 = prompt
+
+// check karo number 1 bada hai ya number 2
+
+// var grade;
+
+// if(perc >=80){
+//     grade = 'A'
+// }
+
+// var name = prompt('Enter Name ')
+// var eng = +prompt('Enter english number')
+// var isl = +prompt('Enter isl number')
+// var chem = +prompt('Enter chem number')
+// var bio = +prompt('Enter bio number')
+// var pst = +prompt('Enter pst number')
+// var total = eng + isl + chem + bio + pst
+// var perc = (total / 500) * 100
+// console.log('perc ==> ',perc)
+// var grade;
+// if (perc >= 80){
+//     grade = 'A+'
+//     alert(grade)
+// }
+// else if(perc >= 70 && perc <80){
+//     grade = 'A'
+//     alert(grade)
+// }
+
+// else if(perc >= 60 && perc <70){
+//     grade = 'B'
+//     alert(grade)
+// }
+
+// else if(perc >= 50 && perc <60){
+//     grade = 'C'
+//     alert(grade)
+// }
+
+// else if(perc >= 40 && perc <50){
+//     grade = 'D'
+//     alert(grade)
+// }
+// else{
+//     grade = 'F'
+//     alert(grade)
+// }
+
+// var name1 = prompt('Enter Name')
+// var eng = +prompt('Enter eng number')
+// var isl = +prompt('Enter isl number')
+// var bio = +prompt('Enter bio number')
+// var chem = +prompt('Enter chem number')
+// var pst = +prompt('Enter pst number')
+// var total = eng + isl + bio + chem + pst
+// var perc = (total / 500) * 100
+// var grade;
+
+// if (perc >= 80 && perc < 101) {
+//     grade = 'A+'
+// }
+// else if (perc >= 70 && perc < 80){
+//     grade = 'A'
+
+// }
+
+// else if (perc >= 60 && perc < 70){
+//     grade = 'B'    
+
+// }
+
+// else if (perc >= 50 && perc < 60){
+//     grade = 'C'    
+
+// }
+
+// else if (perc >= 40 && perc < 50){
+//     grade = 'D'    
+
+// }
+
+// document.write(
+//     '<table border="1px">'+'<tr>'+
+//     '<th>'+'Name'+'</th>'+
+//     '<th>'+'English'+'</th>'+
+//     '<th>'+'Isl'+'</th>'+
+//     '<th>'+'Bio'+'</th>'+
+//     '<th>'+'Chem'+'</th>'+
+//     '<th>'+'Pst'+'</th>'+
+//     '<th>'+'Total'+'</th>'+
+//     '<th>'+'Percentage'+'</th>'+
+//     '<th>'+'Grade'+'</th>'+    
+//     '</tr>'+
+//     '<tr>'+
+//     '<td>'+name1+'</td>'+
+//     '<td>'+eng+'</td>'+
+//     '<td>'+isl+'</td>'+
+//     '<td>'+bio+'</td>'+
+//     '<td>'+chem+'</td>'+
+//     '<td>'+pst+'</td>'+
+//     '<td>'+total+'</td>'+
+//     '<td>'+perc+'</td>'+
+//     '<td>'+grade+'</td>'+
+//     +'</tr>'+
+//     '</table>'
+// )
+
+var i;
+// for (i = 1; i <= 10; i++){
+//     document.write(i+') loop <br>')
+// }
+
+
+// for(var i = 0; i <= 999; i++ ){
+//     document.write(i+'<br>')
+// }
+
+// document.write(2 + ' x ' + 1 + ' = '+ 2*1)
+
+// var user = +prompt('Enter karo apni marzi ka number')
+
+
+// for(var i=1; i<=10; i++){
+//     document.write(user+' x '+i+ ' = '+ user*i+"<br>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
