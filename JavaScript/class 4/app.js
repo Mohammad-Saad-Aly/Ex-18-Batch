cc751e29ec6305965606c7b07bcb0b8cc09e95b5
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-08-05 20:41:29 +0500

    new class

---
+++ JavaScript/class 4/app.js
@@ -0,0 +1,95 @@
+// var arr = ['Saad',true,12,30.4]
+
+// var abc = arr.slice(1)
+
+// document.write(abc)
+// arr.splice(2,2,'24')
+// arr.shift()
+
+// arr.splice(2,1,18)
+
+
+// arr.push('Hamza','Rehman')
+// arr.unshift('Jalal')
+// arr.pop()
+
+// for(var i=0; i <= 10; i++){
+//     document.write(i,') hello world <br>')
+// }
+
+// for (var i = 10; i >= 0; i--){
+//     document.write(i,'<br>')
+// }
+
+
+
+
+
+
+
+// var table = prompt('Enter Table number')
+// for(var i=1; i <= 10; i++){
+//     document.write(table + " x " + i + ' = ' + table*i+"<br>")
+// }
+
+
+
+// var firstArray = ['Hamza', 'Ali', 24, true, 50.8]
+
+
+// for(var i=1; i < firstArray.length; i++){
+//     document.write(firstArray[i]+'<br>')
+// }
+
+
+
+// document.write(firstArray)
+
+
+var firstName = [4,5,6]
+
+
+
+
+
+// console.log(firstName[0])
+// var lastName = ['Ali']
+// var sum =0;
+// for(var i=firstName[0]; i<firstName.length; i++){
+//     document.write(i,'<br>')
+//     // sum += i
+//     // document.write(sum)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaScript/class 5/app.js
@@ -0,0 +1,97 @@
+// var arr = ['Saad','Ali','shaheer']
+
+
+// var a = prompt('first value')
+// var b = prompt('sec value')
+
+
+// arr.unshift(a)
+// arr.push(b)
+
+// document.write(arr)
+
+
+
+// arr.splice(0,4,'Ummara','Hussain')
+// document.write(arr)
+
+// var abc = arr.slice(0)
+
+// var a = prompt()
+// var b = prompt()
+
+// var arr = []
+
+// arr.unshift(a,b)
+
+// console.log(arr)
+
+
+
+// for(var i=1; i<=100; i=i+10){
+//     for(var j=i; j < i+10; j++){
+//         document.write(j," ")
+//     }
+//     document.write('<br>')
+// }
+
+// var arr = ['Taha','Ali']
+// var arr2 = ['Hamza','Jalal']
+
+// for(var i=0; i < arr.length; i++){
+    
+//     for(var j=0; j < arr2.length; j++){
+//         document.write(arr[i]+" "+arr2[j]+"<br>")
+//     }
+
+    // document.write(arr[i]+" "+arr2+"<br>")
+    
+// }
+
+
+for(var i=1; i <=100; i=i+10){
+
+    for(var j = i; j < i+ 10; j++){
+        document.write(j," ")
+    }
+    document.write('<br>')
+
+
+    // document.write(i,'<br>')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
