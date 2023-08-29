521550b78a95536cc97aedd79604b6e365204fb4
mohammad saad ali <mohammadsaadali786.92@gmail.com>
2023-08-29 20:40:15 +0500

    age calculator

---
+++ JavaScript/class 12/app.js
@@ -0,0 +1,41 @@
+// var da = new Date()
+// var damili = da.getTime()
+// var bda = new Date('12 sep, 1999')
+// var bdamili = bda.getTime()
+// var res = damili - bdamili
+// var form = res / (1000 * 60 * 60 * 24 * 365)
+// document.write(form)
+
+
+
+
+
+
+
+
+
+// var tareekh = new Date()
+
+// document.write(tareekh.getMonth())
+
+
+
+var date = new Date()
+var getmili = date.getTime()
+
+var dob = new Date('12 sep, 1999')
+var getmilidob = dob.getTime()
+
+var minus =  getmili - getmilidob
+var formula = minus / (1000 * 60 * 60 * 24 * 365)
+
+console.log(formula)
+
+
+
+
+
+
+
+
+
